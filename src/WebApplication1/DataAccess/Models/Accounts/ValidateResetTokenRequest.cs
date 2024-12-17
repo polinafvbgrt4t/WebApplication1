c3c0be03b07ee5051ab1309e5b1cea262e0de338
polinafvbgrt4t <165783539+polinafvbgrt4t@users.noreply.github.com>
2024-12-17 10:52:51 +0300

    lol

---
+++ src/WebApplication1/DataAccess/Models/Accounts/ValidateResetTokenRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DataAccess.Models.Accounts
+{
+    public class ValidateResetTokenRequest
+    {
+        [Required]
+        public string Token { get; set; }
+    }
+}
